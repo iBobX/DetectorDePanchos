f562b520df453d079a7ff244701c1ac3a5828ce7
Roberto Berrospe <robert@berlinprod.com>
2019-03-25 22:01:04 -0300

    Initial Commit

---
+++ DetectorDePanchos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectorDePanchos
+//
+//  Created by Roberto Antonio Berrospe Machin on 3/25/19.
+//  Copyright © 2019 Ruta Internet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
